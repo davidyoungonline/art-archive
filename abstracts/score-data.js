bf8608a5345de880d533dcdbfe210eb074ff1fee
davidyoungonline <mail@davidyoung.online>
2026-04-23 12:10:27 +0300

    Add files via upload

---
+++ abstracts/score-data.js
@@ -0,0 +1,27 @@
+const composition = {
+  "title": "Abstracts (2004)",
+  "sections": [
+    {
+      "name": "Section 1: Hostile",
+      "audio": "track1.mp3", 
+      "pages": [
+        {"f": "01Hostile00.jpg", "d": 29}, {"f": "01Hostile01.jpg", "d": 11},
+        {"f": "01Hostile02.jpg", "d": 18}, {"f": "01Hostile03.jpg", "d": 29},
+        {"f": "01Hostile04.jpg", "d": 18}, {"f": "01Hostile05.jpg", "d": 11},
+        {"f": "01Hostile06.jpg", "d": 18}, {"f": "01Hostile07.jpg", "d": 29},
+        {"f": "01Hostile08.jpg", "d": 47}, {"f": "01Hostile09.jpg", "d": 29},
+        {"f": "01Hostile10.jpg", "d": 18}, {"f": "01Hostile11.jpg", "d": 11}
+      ]
+    },
+    {
+      "name": "Section 2: Grey",
+      "audio": "track2.mp3",
+      "pages": [
+        {"f": "02Grey00.jpg", "d": 29}, {"f": "02Grey01.jpg", "d": 18},
+        {"f": "02Grey02.jpg", "d": 29}, {"f": "02Grey03.jpg", "d": 47},
+        {"f": "02Grey04.jpg", "d": 29}, {"f": "02Grey05.jpg", "d": 47},
+        {"f": "02Grey06.jpg", "d": 29}
+      ]
+    }
+  ]
+};
